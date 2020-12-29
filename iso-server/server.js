b412c332c75137fe5bcef36348a742b9bb1375e3
Joey <joegiusto@outlook.com>
2020-12-28 19:39:23 -0500

    Presidents Resource
    More work on the Presidents Resource

--- iso-server/server.js
+++ iso-server/server.js
@@ -164,8 +164,8 @@ function connectWithRetryMongo() {
     require('./routes/getStories')(app, db);
     require('./routes/getMyths')(app, db);
 
-    require('./routes/getPresident')(app, db);
-    require('./routes/getPresidents')(app, db);
+    require('./routes/getPresident')(app, db, cache);
+    require('./routes/getPresidents')(app, db, cache);
 
     require('./routes/getSubmissions')(app, db);
     require('./routes/getCanSubmit')(app, db);
--- src/components/News/Presidents/index.js
+++ src/components/News/Presidents/index.js
@@ -2,14 +2,40 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 import { Helmet } from "react-helmet";
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import Chart from 'chart.js';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 
 import * as ROUTES from '../../../constants/routes'; 
 import PresidentCard from './single'
 
 function Presidents(props) {
   const [presidents, setPresidents] = useState([]);
+
+  const [filterSex, setFilterSex] = useState('');
+  const [filterAge, setFilterAge] = useState('');
+  const [filterOrder, setFilterOrder] = useState('Oldest');
+
+  const [activePresidentID, setActivePresidentID] = useState('');
+  const [activePresident, setActivePresident] = useState({});
+  const [modalLoading, setModalLoading] = useState(false);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {setShow(false); setActivePresidentID(''); setActivePresident({}); props.history.push(ROUTES.PRESIDENTS);}
+  const handleShow = () => setShow(true);
+  const handlePresidentSelect = (id) => setActivePresidentID(id);
+
+  function filterOrderRules (presidents) {
+    if (filterOrder === 'Oldest') {
+      return presidents
+    } else {
+      return presidents.reverse()
+    }
+  }
 
   useEffect(() => {
 
@@ -27,8 +53,48 @@ function Presidents(props) {
       console.log(error);
     });
 
+    if (props.match.params.id) {
+      setModalLoading(true);
+      setShow(true);
+
+      axios.post('/api/getPresident', {
+        president_id: props.match.params.id
+      })
+      .then( (response) => {
+        console.log(response)
+        setActivePresident(response.data)
+        setModalLoading(false);
+      })
+      .catch( (error) => {
+        console.log(error);
+      });
+    }
+
   // empty dependency array means this effect will only run once (like componentDidMount in classes)
   }, []);
+
+  useEffect(() => {
+
+    if ( activePresidentID !== '') {
+      console.log("New president details");
+
+      setModalLoading(true);
+
+      axios.post('/api/getPresident', {
+        president_id: activePresidentID
+      })
+      .then( (response) => {
+        console.log(response)
+        setActivePresident(response.data)
+        setModalLoading(false);
+      })
+      .catch( (error) => {
+        console.log(error);
+      });
+    }
+
+  // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, [activePresidentID]);
 
   function renderChartPresidentsAge() {
     // console.log("Render")
@@ -117,6 +183,103 @@ function Presidents(props) {
         <title>Presidents - Articles</title>
       </Helmet>
 
+      <Modal show={show} className="president-modal articles-modal" centered onHide={handleClose}>
+
+        <Modal.Header closeButton>
+          <Modal.Title>President Info</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+
+          <div className="d-flex flex-column flex-lg-row">
+
+            <div className="president-photo mx-auto mx-lg-0 mb-3 mb-lg-0">
+              {modalLoading ?
+              <div className="loading-cover"></div>
+              :
+              <img className="" src={activePresident.photo} alt=""/>
+              }
+            </div>
+
+            <div className="ml-lg-3 mb-3 mb-lg-0">
+
+              <h3>
+                {modalLoading ? <h3 className="text-loading-bar">Loading</h3> : <h3>{`${activePresident.first_name} ${activePresident.last_name}`}</h3>}
+              </h3>
+
+              {!modalLoading &&
+              <>
+              <div>Born: { moment(activePresident.birthday).format("LL") }</div>
+              <div>
+
+                <span className={ ' ' + (activePresident.died ? '' : 'd-none') }> Died: { moment(activePresident.died).format("LL")} </span>
+                <span>(Age: { Math.abs(moment(activePresident.birthday).diff( moment(activePresident.died), 'years')) })</span>
+
+              </div>
+              </>
+              }
+
+            </div>
+
+            <div className="location-photo mb-3 mb-lg-0 mx-auto mx-lg-0 ml-lg-auto">
+              {modalLoading ?
+              <div className="loading-cover"></div>
+              :
+              <div className="ml-auto">
+                <img className="img-fluid" src="https://cdn.articles.media/presidents/george_washinton.jpg" alt=""/>
+              </div>
+              }
+            </div>
+
+          </div>
+
+          <div className="mt-3">
+            {modalLoading ?
+              <>
+              <div className="text-loading-bar w-100 text-white pl-2">Loading</div>
+              <div className="text-loading-bar w-100">Loading</div>
+              <div className="text-loading-bar w-100">Loading</div>
+              </>
+              :
+              activePresident.description
+            }
+          </div>
+
+          <div className="mt-3 mt-lg-5">
+            <button disabled={modalLoading} className="btn btn-articles-light btn-sm active">Accomplishments</button>
+            <button disabled={modalLoading} className="btn btn-articles-light btn-sm">Criticisms</button>
+            <button disabled={modalLoading} className="btn btn-articles-light btn-sm">Pardons</button>
+            <button disabled={modalLoading} className="btn btn-articles-light btn-sm">Gallery</button>
+            <button disabled={modalLoading} className="btn btn-articles-light btn-sm">Vacations</button>
+          </div>
+
+          <div className={"mt-2 " + (modalLoading ? 'd-none' : '')}>
+            <Accordion defaultActiveKey={1}>
+
+              {activePresident.accomplishments?.map((accomplishment, i) =>
+
+                <Card>
+                  <Accordion.Toggle as={Card.Header} style={{cursor: 'pointer'}} eventKey={i + 1}>
+                    {`${accomplishment.accomplishment}`}
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey={i + 1}>
+                    <Card.Body>{accomplishment.description}</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+
+              )}
+
+            </Accordion>
+          </div>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <div className="page-header shadow-sm">
 
         <div className="container py-3">
@@ -133,20 +296,21 @@ function Presidents(props) {
             <div className="d-flex flex-column flex-lg-row">
 
               <div className="filter-section">
-                <div className="label">Gender:</div>
+                <div className="label">Sex:</div>
                 <div>
-                  <button className="btn btn-articles-light btn-sm">Male</button>
-                  <button className="btn btn-articles-light btn-sm" disabled={true}>Female</button>
+                  <button onClick={() => filterSex === 'Male' ? setFilterSex('') : setFilterSex('Male')} className={"btn btn-articles-light btn-sm " + (filterSex === 'Male' ? 'active' : '') }>Male</button>
+                  <button onClick={() => filterSex === 'Female' ? setFilterSex('') : setFilterSex('Female')} className={"btn btn-articles-light btn-sm " + (filterSex === 'Female' ? 'active' : '') }>Female</button>
                 </div>
               </div>
   
               <div className="filter-section ml-lg-4">
-                <div className="label">Age:</div>
+                <div className="label">Term Start Age:</div>
                 <div className="buttons">
-                  <button className="btn btn-articles-light btn-sm">35-45</button>
-                  <button className="btn btn-articles-light btn-sm" disabled={true}>55-65</button>
-                  <button className="btn btn-articles-light btn-sm" disabled={true}>65-75</button>
-                  <button className="btn btn-articles-light btn-sm" disabled={true}>75-85</button>
+                  <button onClick={() => filterAge === '35-44' ? setFilterAge('') : setFilterAge('35-44')} className={"btn btn-articles-light btn-sm " + (filterAge === '35-44' ? 'active' : '')}>35-44</button>
+                  <button onClick={() => filterAge === '45-54' ? setFilterAge('') : setFilterAge('45-54')} className={"btn btn-articles-light btn-sm " + (filterAge === '45-54' ? 'active' : '')}>45-54</button>
+                  <button onClick={() => filterAge === '55-64' ? setFilterAge('') : setFilterAge('55-64')} className={"btn btn-articles-light btn-sm " + (filterAge === '55-64' ? 'active' : '')}>55-64</button>
+                  <button onClick={() => filterAge === '65-74' ? setFilterAge('') : setFilterAge('65-74')} className={"btn btn-articles-light btn-sm " + (filterAge === '65-74' ? 'active' : '')}>65-74</button>
+                  <button onClick={() => filterAge === '75-84' ? setFilterAge('') : setFilterAge('75-84')} className={"btn btn-articles-light btn-sm " + (filterAge === '75-84' ? 'active' : '')}>75-84</button>
                 </div>
               </div>
   
@@ -167,8 +331,8 @@ function Presidents(props) {
               <div className="filter-section ml-lg-4">
                 <div className="label">Order:</div>
                 <div className="buttons">
-                  <button className="btn btn-articles-light btn-sm">Oldest</button>
-                  <button className="btn btn-articles-light btn-sm" disabled={true}>Newest</button>
+                  <button onClick={() => setFilterOrder('Oldest') } className={"btn btn-articles-light btn-sm " + (filterOrder === 'Oldest' ? 'active' : '')}>Oldest</button>
+                  <button onClick={() => setFilterOrder('Newest') } className={"btn btn-articles-light btn-sm " + (filterOrder === 'Newest' ? 'active' : '')}>Newest</button>
                 </div>
               </div>
 
@@ -190,18 +354,20 @@ function Presidents(props) {
       <div className="presidents-grid-container container">
 
         <div className="presidents-grid py-3">
-          {presidents.map((president) => 
 
-            <Link to={`${ROUTES.PRESIDENTS}/${president._id}`}>
+          {filterOrderRules(presidents).map((president) => 
+            <Link onClick={ () => { setShow(true); setActivePresidentID(president._id); } } to={`${ROUTES.PRESIDENTS}/${president._id}`}>
               <PresidentCard president={president}/>
             </Link>
-
           )}
+
         </div>
 
       </div>
 
-      <div className="charts-container container mb-3">
+      <hr/>
+
+      <div className="charts-container container mb-3 mb-lg-5">
         <div className="row">
 
           <div className="col-md-6">
@@ -239,6 +405,8 @@ function Presidents(props) {
         </div>
       </div>
 
+      <hr/>
+
       <div className="container mb-3">
         <button className="btn btn-articles-light">Download JSON</button>
       </div>
@@ -247,4 +415,4 @@ function Presidents(props) {
   );
 }
 
-export default Presidents;
+export default withRouter(Presidents);
