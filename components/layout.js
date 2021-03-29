ad5dce6490de620cb53edab9859533cd91f3960f
Joey Giusto <joegiusto@outlook.com>
2021-03-28 20:27:24 -0400

    Bump

--- components/layout.js
+++ components/layout.js
@@ -22,14 +22,36 @@ export default function Layout(
         <div style={{minHeight: '100vh'}} className={`d-flex flex-column ${colorModeDark ? 'dark-mode' : ''} `}>
 
             <Head>
+
                 <title>{title}</title>
                 <meta charSet="utf-8" />
-                <link rel="icon" href="/favicon.ico" />
+                {/* <meta name="description" content="Description" /> */}
+                <meta name="description" content="A political organization and platform working to make America a better place for the people through avenues of transparency, clothing, news, and politics."/>
+                {/* <link rel="icon" href="/favicon.ico" /> */}
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
                 {/* <script src="https://kit.fontawesome.com/d1a2586c98.js" crossorigin="anonymous"></script> */}
+
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link rel="stylesheet" href="https://use.typekit.net/wvo0uze.css"/>
-                <script src="https://kit.fontawesome.com/d1a2586c98.js" crossOrigin="anonymous"></script>
+
+                <link rel="manifest" href="/manifest.json" />
+                <link
+                    href="/icons/favicon-16x16.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                />
+                <link
+                    href="/icons/favicon-32x32.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                />
+                <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+                <meta name="theme-color" content="#000000" />
+
+                {/* <script src="https://kit.fontawesome.com/d1a2586c98.js" crossOrigin="anonymous"></script> */}
             </Head>
 
             <SideMenu
